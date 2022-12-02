62cf9c22fa66c43a81c37cc5e69a68a941d80f47
arsenmamataliev <a.mamataliev93@gmail.com>
2022-12-02 18:52:52 +0600

    Fade deleted from Mywork section

--- src/components/MyWorks/MyWork/MyWork.js
+++ src/components/MyWorks/MyWork/MyWork.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import style from './MyWork.module.scss';
-import Fade from 'react-awesome-reveal';
 import { Link } from 'react-router-dom';
 import Tilt from 'react-parallax-tilt';
 
 export const MyWork = (props) => {
     return (
-        <Fade top>
-            <div className={style.preview} style={props.style}>
-                <img src={props.imgLink} alt="avatar" />
+        <div className={style.preview} style={props.style}>
+            <img src={props.imgLink} alt="avatar" />
                 <div className={style.description}>
                     <h3 className={style.title}>{props.title}</h3>
                     <div className={style.stack}>
@@ -21,8 +19,7 @@ export const MyWork = (props) => {
                     <div className={style.linkContainer}>
                         <Link to={props.link} className={style.link}>visit</Link>
                     </div>     
-                </div>       
-            </div> 
-        </Fade>
+                </div>           
+        </div>    
     )
 }
